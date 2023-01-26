02bfdc927badb827ce807772ea509ce7f849b76a
Rerison Barros <rerisonmcb@gmail.com>
2023-01-26 13:20:53 -0300

    feat: adição do método 'getClassName'
     No ramo main
     Mudanças a serem submetidas:
    	modified:   Lib/Core/Autoloader.php
    
     Arquivos não monitorados:
    	Lib/Core/Route.php
    	autoload.php
    	index.php

--- Lib/Core/Autoloader.php
+++ Lib/Core/Autoloader.php
@@ -15,9 +15,9 @@ class Autoloader{
 
 	public function load($class){
 
-		$this->class = $class;
+		$this->class = str_replace("\\", "/", $class);
 
-		echo "<pre>", var_dump($this->rootPath()), "</pre>";
+		echo "<pre>", var_dump($this->rootPath(), $this->getClassName()), "</pre>";
 	}
 
 	public function rootPath(){
@@ -25,6 +25,13 @@ class Autoloader{
 		$rootPath = strstr(__DIR__, $this->rootFolder, true).$this->rootFolder."/";
 
 		return $rootPath;
+	}
+
+	public function getClassName(){
+
+		$className = substr(strrchr($this->class, "/"),1);
+
+		return $className;
 	}
 
 }
